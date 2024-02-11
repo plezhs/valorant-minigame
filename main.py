f06ec2cc7dafc28f779c7e1a92d827fd1fb15471
plezhs <pramasns@gmail.com>
2024-02-12 02:24:24 +0900

    point decrease system

--- main.py
+++ main.py
@@ -106,50 +106,79 @@ answer = dict()
 async def 스킬문제(ctx):
     global answer
     global Lresult
-    i = random.sample(range(0,len(Lresult[0])-1),1)[0]
-    agentsIcon = Lresult[5][i]
+    if answer.get(ctx.author) != None:
+        i = random.sample(range(0,len(Lresult[0])-1),1)[0]
+        agentsIcon = Lresult[5][i]
 
-    abilityQ = Lresult[1]
-    abilityE = Lresult[2]
-    abilityC = Lresult[3]
-    abilityX = Lresult[4]
+        abilityQ = Lresult[1]
+        abilityE = Lresult[2]
+        abilityC = Lresult[3]
+        abilityX = Lresult[4]
 
-    ability = list()
-    ability.append(abilityQ)
-    ability.append(abilityE)
-    ability.append(abilityC)
-    ability.append(abilityX)
+        ability = list()
+        ability.append(abilityQ)
+        ability.append(abilityE)
+        ability.append(abilityC)
+        ability.append(abilityX)
 
-    IconabilityQ = Lresult[6]
-    IconabilityE = Lresult[7]
-    IconabilityC = Lresult[8]
-    IconabilityX = Lresult[9]
-    
-    Iconability = list()
-    Iconability.append(IconabilityQ)
-    Iconability.append(IconabilityE)
-    Iconability.append(IconabilityC)
-    Iconability.append(IconabilityX)
+        IconabilityQ = Lresult[6]
+        IconabilityE = Lresult[7]
+        IconabilityC = Lresult[8]
+        IconabilityX = Lresult[9]
+        
+        Iconability = list()
+        Iconability.append(IconabilityQ)
+        Iconability.append(IconabilityE)
+        Iconability.append(IconabilityC)
+        Iconability.append(IconabilityX)
 
-    r=random.sample(range(0,4),1)[0]
-    temp = ability[r][i]
-    aw = temp.replace(" ","")
-    name = ctx.message.author.name
-    answer[ctx.message.author] = aw
-    name = name.title()
-    embed1 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description=f"{name}님의 문제입니다.",title=f"이스킬의 이름은?")
-    embed1.set_image(url=Iconability[r][i])
-    embed1.set_thumbnail(url=agentsIcon)
-    embed1.add_field(name="※주의 사항※",value=f"띄어쓰기는 생략해주세요.\n(이 게임에 도움을 준 사람: 유건우)", inline=False) #inline이 False라면 다음줄로 넘깁니다.
-    await ctx.send(embed=embed1)
-    with open(f'{rt}.log.txt', 'a',encoding='UTF-8') as f:
-        f.write(f"[{time}] {ctx.message.author} received a quiz. Quiz's answer was {aw}.\n")
+        r=random.sample(range(0,4),1)[0]
+        temp = ability[r][i]
+        aw = temp.replace(" ","")
+        name = ctx.message.author.name
+        answer[ctx.message.author] = aw
+        name = name.title()
+        embed1 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description=f"{name}님의 문제입니다.",title=f"이스킬의 이름은?")
+        embed1.set_image(url=Iconability[r][i])
+        embed1.set_thumbnail(url=agentsIcon)
+        embed1.add_field(name="※주의 사항※",value=f"띄어쓰기는 생략해주세요.\n(이 게임에 도움을 준 사람: 유건우)", inline=False) #inline이 False라면 다음줄로 넘깁니다.
+        await ctx.send(embed=embed1)
+        with open(f'{rt}.log.txt', 'a',encoding='UTF-8') as f:
+            f.write(f"[{time}] {ctx.message.author} received a quiz. Quiz's answer was {aw}.\n")
+
+    else:
+        await ctx.send("이전문제를 먼저 풀어주세요.\n패널티로 25점이 차감됩니다.")
+        with open('.\\db.json','r+',encoding='UTF-8') as f:
+                jjj = json.load(f)
+                nnn = {
+                    f"{ctx.author.id}":{"points":25}
+                }
+                try:
+                    dump = jjj["Points"][0][f"{ctx.author.id}"]["points"] -25
+                    abc = {}
+                    abc["Points"] = []
+                    abc['Points'].append({f"{ctx.author.id}":{"points":dump}})
+                    print(abc) 
+                except KeyError:
+                    dump = 100
+                    abc = {}
+                    abc["Points"] = []
+                    abc['Points'].append(nnn)
+                except IndexError:
+                    dump = 100
+                    abc = {}
+                    abc["Points"] = []
+                    abc['Points'].append(nnn)
+                f.seek(0)
+                json.dump(abc, f, indent=4)
+
+
 @bot.command()
 async def 스킬정답(ctx,a=None):
     global answer
     if answer.get(ctx.message.author) != None:
         if a.title() == answer.get(ctx.message.author):
-            await ctx.send("정답입니다!!!")
+            await ctx.send("정답입니다!!! 100점이 지급되었습니다.")
             with open(f'{rt}.log.txt', 'a',encoding='UTF-8') as f:
                 f.write(f"[{time}] {ctx.message.author} got a quiz right. Quiz's answer was {answer.get(ctx.message.author)}. {ctx.author} got 100 points.\n")
             with open('.\\db.json','r+',encoding='UTF-8') as f:
@@ -177,10 +206,33 @@ async def 스킬정답(ctx,a=None):
                 json.dump(abc, f, indent=4)
             answer.pop(ctx.message.author)
         else:
-            await ctx.send(f"아...\n정답은 {answer.get(ctx.message.author)} 이었습니다...")
+            await ctx.send(f"아...\n패널티로 50점이 차감되었습니다....")
+            with open('.\\db.json','r+',encoding='UTF-8') as f:
+                jjj = json.load(f)
+                nnn = {
+                    f"{ctx.author.id}":{"points":50}
+                }
+                try:
+                    dump = jjj["Points"][0][f"{ctx.author.id}"]["points"] - 50
+                    abc = {}
+                    abc["Points"] = []
+                    abc['Points'].append({f"{ctx.author.id}":{"points":dump}})
+                    print(abc) 
+                except KeyError:
+                    dump = 50
+                    abc = {}
+                    abc["Points"] = []
+                    abc['Points'].append(nnn)
+                except IndexError:
+                    dump = 50
+                    abc = {}
+                    abc["Points"] = []
+                    abc['Points'].append(nnn)
+                f.seek(0)
+                json.dump(abc, f, indent=4)
             with open(f'{rt}.log.txt', 'a',encoding='UTF-8') as f:
                 f.write(f"[{time}] {ctx.message.author} got a quiz wrong. {ctx.message.author}'s answer was {a}. Quiz's answer was {answer.get(ctx.message.author)}.\n")
-            answer.pop(ctx.message.author)
+            # answer.pop(ctx.message.author)
     else:
         await ctx.send("먼저 문제를 받아주세요.")
         with open(f'{rt}.log.txt', 'a',encoding='UTF-8') as f:
